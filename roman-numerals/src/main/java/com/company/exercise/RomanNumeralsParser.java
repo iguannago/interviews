791edf9ae3e0dd4b564f13526975b4a81b99a7db
david.crespo <david.crespo@hmcts.net>
2022-09-04 13:05:27 +0100

    refactor code and add some more unit scenarios

--- roman-numerals/src/main/java/com/company/exercise/RomanNumeralsParser.java
+++ roman-numerals/src/main/java/com/company/exercise/RomanNumeralsParser.java
@@ -2,6 +2,9 @@ package com.company.exercise;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.IntStream;
 
 public class RomanNumeralsParser {
 
@@ -17,25 +20,38 @@ public class RomanNumeralsParser {
     }
 
     int parse(String romanNumerals) {
-        int count = 0;
-        Integer value = 0;
-        for (int i = 0; i < romanNumerals.length(); i++) {
-            char currentChar = romanNumerals.charAt(i);
-            value += romans.get(String.valueOf(currentChar));
-            char previousChar = 0;
+        AtomicInteger value = new AtomicInteger(0);
+        IntStream.range(0, romanNumerals.length()).forEach(index -> {
+            char currentChar = romanNumerals.charAt(index);
+            value.updateAndGet(v -> v + romans.get(String.valueOf(currentChar)));
 
-            if (i > 0) {
-                previousChar = romanNumerals.charAt(i - 1);
-            }
-            if (currentChar == previousChar) {
-                count++;
-            }
-            if (count == 3) {
-                throw new IllegalArgumentException("more than three consecutive chars");
-            }
+            letterCanNotBeRepeatedThreeTimesCheck(romanNumerals);
+        });
 
-        }
-
-        return value;
+        return value.get();
     }
+
+
+    private void letterCanNotBeRepeatedThreeTimesCheck(String romanNumerals) {
+        if (romanNumerals.length() > 3) {
+            AtomicInteger count = new AtomicInteger(0);
+            AtomicReference<Character> currentChar = new AtomicReference<>();
+            AtomicReference<Character> previousChar = new AtomicReference<>('0');
+
+            IntStream.range(0, romanNumerals.length()).forEach(index -> {
+                currentChar.set(romanNumerals.charAt(index));
+                if (index > 0) {
+                    previousChar.set(romanNumerals.charAt(index - 1));
+                }
+                if (currentChar.get().equals(previousChar.get())) {
+                    count.getAndIncrement();
+                }
+            });
+
+            if (count.get() == 3) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
 }
--- roman-numerals/src/test/java/com/company/exercise/RomanNumeralsParserTest.java
+++ roman-numerals/src/test/java/com/company/exercise/RomanNumeralsParserTest.java
@@ -1,50 +1,68 @@
 package com.company.exercise;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class RomanNumeralsParserTest {
-  private final RomanNumeralsParser romanNumeralsParser = new RomanNumeralsParser();
+    private final RomanNumeralsParser romanNumeralsParser = new RomanNumeralsParser();
 
-  @Test
-  public void shouldReturn1ForI() {
-    int result = romanNumeralsParser.parse("I");
+    @Test
+    public void shouldReturn1ForI() {
+        int result = romanNumeralsParser.parse("I");
 
-    assertEquals(1, result);
-  }
+        assertEquals(1, result);
+    }
 
-  @Test
-  public void shouldReturn5ForV() {
-    assertEquals(5, romanNumeralsParser.parse("V"));
-  }
+    @Test
+    public void shouldReturn5ForV() {
+        assertEquals(5, romanNumeralsParser.parse("V"));
+    }
 
-  @Test
-  public void shouldReturn10ForX() {
-    assertEquals(10, romanNumeralsParser.parse("X"));
-  }
-  @Test
-  public void shouldReturn50ForL() {
-    assertEquals(50, romanNumeralsParser.parse("L"));
-  }
-  @Test
-  public void shouldReturn100ForC() {
-    assertEquals(100, romanNumeralsParser.parse("C"));
-  }
-  @Test
-  public void shouldReturn1000ForM() {
-    assertEquals(1000, romanNumeralsParser.parse("M"));
-  }
+    @Test
+    public void shouldReturn10ForX() {
+        assertEquals(10, romanNumeralsParser.parse("X"));
+    }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void shouldReturnErrorForXXXX() {
-    romanNumeralsParser.parse("XXXX");
-  }
+    @Test
+    public void shouldReturn50ForL() {
+        assertEquals(50, romanNumeralsParser.parse("L"));
+    }
 
-  @Test
-  public void shouldReturn30ForXXX() {
-    romanNumeralsParser.parse("XXX");
-  }
+    @Test
+    public void shouldReturn100ForC() {
+        assertEquals(100, romanNumeralsParser.parse("C"));
+    }
+
+    @Test
+    public void shouldReturn1000ForM() {
+        assertEquals(1000, romanNumeralsParser.parse("M"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldReturnErrorForXXXX() {
+        romanNumeralsParser.parse("XXXX");
+    }
+
+    @Test
+    public void shouldReturn30ForXXX() {
+        romanNumeralsParser.parse("XXX");
+    }
+
+    @Test
+    public void shouldReturn6ForVI() {
+        assertEquals(6, romanNumeralsParser.parse("VI"));
+    }
+    @Test
+    public void shouldReturn7ForVII() {
+        assertEquals(7, romanNumeralsParser.parse("VII"));
+    }
+    @Test
+    public void shouldReturn70ForLXX() {
+        assertEquals(70, romanNumeralsParser.parse("LXX"));
+    }
+    @Test
+    public void shouldReturn1200ForMCC() {
+        assertEquals(1200, romanNumeralsParser.parse("MCC"));
+    }
 }
